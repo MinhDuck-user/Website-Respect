9fb76c22981fdca9ba9240b37353f9d9bc87e67d
MinhDuck-user <ducnmfhl31907@gmail.com>
2025-04-28 20:43:48 +0700

    Update chatbot.js

--- backend/public/chatbot.js
+++ backend/public/chatbot.js
@@ -1,5 +1,4 @@
-// public/chatbot.js
-const botSocket = io('/chat/chatbot');  // Kết nối đúng namespace bot
+const botSocket = io('/chat/chatbot');
 
 document.getElementById('bot-form').addEventListener('submit', e => {
   e.preventDefault();
@@ -12,12 +11,10 @@ document.getElementById('bot-form').addEventListener('submit', e => {
   }
 });
 
-// Khi nhận phản hồi từ bot
 botSocket.on('bot reply', reply => {
   addMessage('Bot', reply);
 });
 
-// Hàm thêm tin nhắn vào giao diện
 function addMessage(sender, text) {
   const li = document.createElement('li');
   li.innerHTML = `<strong>${sender}:</strong> ${text}`;
